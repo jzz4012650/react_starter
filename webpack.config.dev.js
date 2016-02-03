1f08493b76f3e3359ba43420fdfdc80af30c6b0f
Jin Zhenzu <jinzhenzu@360.cn>
2016-02-03 11:40:22 +0800

    add hot-dev-server  &  minimize js/css build

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,10 @@ var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
+    devtool: 'eval',
     entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
         './src/main.jsx'
     ],
     output: {
@@ -14,14 +17,17 @@ module.exports = {
     module: {
         loaders: [{
             test: /\.less$/,
-            loader: ExtractTextPlugin.extract("css!less")
+            loader: ExtractTextPlugin.extract(
+                "css?sourceMap!less?sourceMap")
         }, {
             test: /\.jsx?$/,
+            // exclude: /(node_modules|bower_components)/,
             loaders: ['react-hot', 'babel'],
             include: path.join(__dirname, 'src')
         }]
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
         new ExtractTextPlugin("[name].css")
     ]
 }
