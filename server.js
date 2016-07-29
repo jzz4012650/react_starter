fdbcf43d5526b27ac52db49ef68383e304a01dbe
金振祖 <jinzhenzu@360.cn>
2016-07-29 11:31:49 +0800

    update webpack config in single file, update react to v15

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config.dev');
+var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    stats: { colors: true }
-}).listen(3000, 'localhost', function(err, result) {
+var server = new WebpackDevServer(webpack(config), config.devServer);
+
+server.listen(config.devServer.port, 'localhost', function(err, result) {
     if (err) {
         console.log(err);
     }
 
-    console.log('Listening at localhost:3000');
+    console.log('Listening at localhost:' + config.devServer.port);
 });
