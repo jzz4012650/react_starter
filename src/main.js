ca96a80382e1c9bebb812f654fd37c506ec4765a
金振祖 <jinzhenzu@360.cn>
2016-08-08 11:32:22 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,8 @@
+require('./main.less');
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+
+ReactDom.render(<Main/>, document.querySelector('#container'));
+
